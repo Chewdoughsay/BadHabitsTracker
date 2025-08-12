af1079fe1fea78c91af284f6994e4a38d8c4ac78
Tudose <vasile.tudose@student.unitbv.ro>
2025-08-12 23:17:22 +0300

    Usecaseuri pt: - user cu habitsurile lui - dashboard user - statistici dashboard user

---
+++ app/src/main/java/com/example/badhabitstracker/domain/usecase/GetUserWithHabitsUseCase.kt
@@ -0,0 +1,29 @@
+package com.example.badhabitstracker.domain.usecase
+
+import com.example.badhabitstracker.domain.model.UserWithHabits
+import com.example.badhabitstracker.domain.repository.HabitRepository
+import com.example.badhabitstracker.domain.repository.UserRepository
+import kotlinx.coroutines.flow.first
+
+/**
+ * user cu toate habiturile lui
+ */
+class GetUserWithHabitsUseCase(
+    private val userRepository: UserRepository,
+    private val habitRepository: HabitRepository
+) : BaseUseCaseWithCurrentUserNoParams<UserWithHabits>(userRepository) {
+
+    override suspend fun execute(userId: Long): UserWithHabits {
+        // get user
+        val user = userRepository.getUserById(userId)
+            ?: throw IllegalStateException("User not found")
+
+        // get habits for user
+        val habits = habitRepository.getAllHabits(userId).first()
+
+        return UserWithHabits(
+            user = user,
+            habits = habits
+        )
+    }
+}
